a805b6200afa1a2eba4ba04dddbd0d84ce2fad12
Pritesh Rane <priteshrane01@gmail.com>
2020-08-29 10:57:55 +0530

    Sealed classes example from document

---
+++ src/classesandobjects/SealedClasses.kt
@@ -0,0 +1,26 @@
+package classesandobjects
+
+sealed class Expr {
+}
+
+data class Const(val number: Double) : Expr()
+
+data class Sum(val e1: Expr, val e2: Expr) : Expr()
+
+object NotANumber : Expr()
+
+fun eval(expr: Expr): Double = when(expr) {
+    is Const -> expr.number
+    is Sum -> eval(expr.e1) + eval(expr.e2)
+    NotANumber -> Double.NaN
+}
+
+fun sealedClasses() {
+    
+}
+
+fun main() {
+    println("Sealed class example: ")
+    sealedClasses()
+    println()
+}
